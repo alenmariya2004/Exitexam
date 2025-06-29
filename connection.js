7a337e31fdd89476435cc2225544108dfbf9eab5
alenmariya2004 <161220375+alenmariya2004@users.noreply.github.com>
2025-06-29 16:43:41 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 //Write missing code here
 mongoose
   .connect(
-   "mongodb+srv://alenmariya102:alen@cluster0.osowb4x.mongodb.net/ict1?retryWrites=true&w=majority&appName=Cluster0"
+   "mongodb+srv://****:****@cluster0.osowb4x.mongodb.net/ict1?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("Connected to DB");
